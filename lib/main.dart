5a5dfa0c5e15c75cfc59ccc3264682febfb55404
limminghong <minghonglim@gmail.com>
2020-07-29 20:16:38 +0800

    Adding a todo list

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,62 @@ class TodoList extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    if (task.length > 0) {
+      setState(() {
+        _todoItems.add(task);
+      });
+    }
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+        // MaterialPageRoute will automatically animate the screen entry, as well
+        // as adding a back button to close it
+        new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+          appBar: new AppBar(title: new Text('Add a new task')),
+          body: new TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              _addTodoItem(val);
+              Navigator.pop(context); // Close the add todo screen
+            },
+            decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)),
+          ));
+    }));
+  }
+
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(title: new Text(todoText));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Todo List'),
       ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _pushAddTodoScreen,
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)),
     );
   }
 }
